978e1c9758593bc0996820122ff583361f98319b
Alex Hunter <alex.hunter4004@gmail.com>
2025-12-07 18:54:21 +1100

    add deploy workflow

--- my-react-app/vite.config.ts
+++ my-react-app/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/repo-name/',
+  base: '/website-resume/',
   plugins: [
     react({
       babel: {
